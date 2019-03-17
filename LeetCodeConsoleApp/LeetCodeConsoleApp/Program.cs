40dffc5ea8a61e277038d9af0e4517dcf2b0c911
Nan Huo <v-nahuo@microsoft.com>
2019-03-17 19:24:14 +0900

    Update reverse string function

--- LeetCodeConsoleApp/LeetCodeConsoleApp/Program.cs
+++ LeetCodeConsoleApp/LeetCodeConsoleApp/Program.cs
@@ -10,9 +10,15 @@ namespace LeetCodeConsoleApp
     {
         public static void Main(string[] args)
         {
-            int[] testArray = new int[10] { 1, 1, 3, 3, 4, 5, 6, 8, 19, 29 };
-            int sum = 14;
-            SearchAscArray.SearchAscArrayAndPrint(testArray, sum);
+            ////Given a sorted ascending array and a number x, find the pair in array whose sum is closest to x
+            //int[] testArray = new int[10] { 1, 1, 3, 3, 4, 5, 6, 8, 19, 29 };
+            //int sum = 14;
+            //SearchAscArray.SearchAscArrayAndPrint(testArray, sum);
+            //Console.ReadLine();
+
+            //Reverse Hello World to World Hello
+            string orgionalString = "Hello World";
+            ReverseString.ReverseHelloWorldString(orgionalString);
             Console.ReadLine();
         }
     }
---
+++ LeetCodeConsoleApp/LeetCodeConsoleApp/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeConsoleApp
+{
+    public static class ReverseString
+    {
+        /// <summary>
+        /// Reverse Hello World into World Hello
+        /// </summary>
+        /// <param name="inputString"></param>
+        public static void ReverseHelloWorldString(string inputString)
+        {
+            string[] tempResult = inputString.Split();
+            string outputString = tempResult[1] +" "+ tempResult[0];
+            Console.WriteLine("The final result is {0}", outputString);
+        }
+    }
+}
